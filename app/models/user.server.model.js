04687beba037577d69becdf25df289cb3ef9fb4d
Keenan Karich <kkarich@yahoo.com>
2015-05-07 13:35:17 +0000

    updated sign in, sign on, and edit profile implementation

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -25,28 +25,9 @@ var validateLocalStrategyPassword = function(password) {
  * User Schema
  */
 var UserSchema = new Schema({
-	firstName: {
-		type: String,
-		trim: true,
-		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your first name']
-	},
-	lastName: {
-		type: String,
-		trim: true,
-		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your last name']
-	},
 	displayName: {
 		type: String,
 		trim: true
-	},
-	email: {
-		type: String,
-		trim: true,
-		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your email'],
-		match: [/.+\@.+\..+/, 'Please fill a valid email address']
 	},
 	username: {
 		type: String,
