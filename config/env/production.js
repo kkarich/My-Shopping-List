df76c01444ba5679844177debb5442121976ef9c
Keenan Karich <kkarich@yahoo.com>
2015-05-07 14:21:21 +0000

    updated production env for heroku

--- config/env/production.js
+++ config/env/production.js
@@ -6,8 +6,8 @@ module.exports = {
 		lib: {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-				'public/lib/font-awesome/css/font-awesome.css'
+				'public/lib/flat-ui/dist/css/flat-ui.min.css',
+				'public/lib/font-awesome/css/font-awesome.min.css'
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
@@ -18,7 +18,9 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/angular-socket-io/socket.js',
+				'socket.io/socket.io.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
