06ac274a5fdc5fc262990f5961a2fa57d99558b2
Keenan Karich <kkarich@yahoo.com>
2015-05-07 14:02:01 +0000

    updated production env for heroku

--- public/modules/core/services/socket.js
+++ public/modules/core/services/socket.js
@@ -5,7 +5,7 @@ angular.module('core').factory('Socket', ['socketFactory',
     function(socketFactory) {
         return socketFactory({
             prefix: '',
-            ioSocket: io.connect('http://ukkk6aef799c.keenanmkarich.koding.io:3000')
+            ioSocket: io.connect('http://my-shopping-list2.herokuapp.com')
         });
     }
 ]);
